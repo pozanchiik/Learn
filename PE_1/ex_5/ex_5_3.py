818306af587656deef22ddfa7cce11f2e1dfd9a3
pozanchiik <149064104+pozanchiik@users.noreply.github.com>
2025-09-25 20:14:15 +0300

    Create ex_5_3.py

---
+++ PE_1/ex_5/ex_5_3.py
@@ -0,0 +1,9 @@
+import math
+from math import pi
+
+a = float(input("a = "))
+m_1 = float(input("m = "))
+m_2 = float(input("m = "))
+p = float(input("p = "))
+
+print(f"g = {4 * math.pi ** 2 * (a ** 3 /(p ** 2 * (m_1 + m_2)) )}")
