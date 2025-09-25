6656995558237124ef76d971b3fa424f1a95f454
pozanchiik <149064104+pozanchiik@users.noreply.github.com>
2025-09-25 20:14:30 +0300

    Create ex_2.py

---
+++ PE_1/ex_2.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+print(f'{a+b}\n{a-b}\n{a*b}\n{(a+b)/2}\n{sqrt(a*b)}\n{abs(a-b)}\n{max(a,b)}\n{min(a,b)}\n{a**a+b**b}\n{(a+b)**2}\n{a**b}')
